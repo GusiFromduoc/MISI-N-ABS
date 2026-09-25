1f2163cc7c00f970f563145b6cd4394d3cac7af1
Alumno Plaza Vespucio <pv-alumno@duoc.acad>
2026-09-24 21:46:02 -0300

    fin

--- Main.java
+++ Main.java
@@ -5,6 +5,8 @@ public class Main {
         BrigadaTecnica tecnico1 = new BrigadaTecnica("Miguel");
         DronRescate dron1 = new DronRescate ("001");
 
+        Volador volador = new DronRescate("Dron Aéreo");
+        Reparador reparador = new BrigadaTecnica("Equipo Técnico");
         medico1.atenderEmergencia();
         tecnico1.atenderEmergencia();
         dron1.atenderEmergencia();
@@ -15,6 +17,17 @@ public class Main {
 
         dron1.volar();
         tecnico1.reparar();
+        volador.volar();
+        reparador.reparar();
 
+        EquipoEmergencia equipo1 = new BrigadaMedica("Brigada Médica");
+
+        EquipoEmergencia equipo2 = new BrigadaTecnica("Brigada Técnica");
+
+        EquipoEmergencia equipo3 = new DronRescate("Dron de Rescate");
+
+        equipo1.atenderEmergencia();
+        equipo2.atenderEmergencia();
+        equipo3.atenderEmergencia();
     }
 }
